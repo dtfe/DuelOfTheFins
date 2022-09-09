7aafb8c44a578b37a83fa532354d7100d7cdf0c1
dtfe <355976@student.nord.no>
2022-09-09 12:42:02 +0200

    Inputsystem adjustments & Character Controller

--- Assets/_Assets/_Scripts/playerController.cs
+++ Assets/_Assets/_Scripts/playerController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    private float movementX;
+    private float movementY;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>(); //Sets rb2d to be the component of the rigidbody of whatever gameobject this script is on
+    }
+
+    private void onMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb2d.AddForce(movement * speed);
     }
 }
