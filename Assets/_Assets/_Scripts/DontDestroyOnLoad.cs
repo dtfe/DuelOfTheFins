594d1f8dff21a43689ce1e12ec0b00fac51cd3e8
dtfe <355976@student.nord.no>
2022-11-18 10:51:48 +0100

    Space Map variants 2-4, Players slow down when they dash into water bubbles, players despawn when entering main menu

--- Assets/_Assets/_Scripts/DontDestroyOnLoad.cs
+++ Assets/_Assets/_Scripts/DontDestroyOnLoad.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
